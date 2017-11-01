8c180a2a1f116e5c2c0a8433ac82395c7ef70174
Zach Spangler <zaspangler@gmail.com>
2017-11-01 10:37:32 -0600

    Updated additional css prepared to format pictures

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 				<div class="container">
 
 					<!-- WELCOME IMAGE -->
-					<img src="images/welcome.gif" alt="welcome to my homepage">
+					<img src="images/welcome.gif" alt="welcome to my homepage" class="welcomepic">
 
 					<div class="instructions-wrapper">
 						<div class="instructions-content">
@@ -37,7 +37,7 @@
 								<li>Have fun! The solution will be demonstrated in class. <small>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</small></li>
 							</ol>
 						</div>
-						<img src="images/captain-janeway.png" alt="captain janeway" />
+						<img src="images/captain-janeway.png" alt="captain janeway" height="25%" width="25%"/>
 						<div class="clearfix"></div>
 					</div><!--/.instructions-->
 				</div><!--/.container-->
@@ -46,7 +46,7 @@
 			<!-- ABOUT ME -->
 			<section class="about-me">
 				<div class="container">
-					<img src="images/stunning.jpg" alt="captain picard" />
+					<img src="images/stunning.jpg" alt="captain picard" height="25%" width="25%"/>
 					<h2>A Day in the Life...</h2>
 					<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
 					<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
